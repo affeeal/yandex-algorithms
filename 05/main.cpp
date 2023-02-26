51d4814165ac1dd4e4ecc496d7cb75ad321145de
Ilya Afanasyev <ilya.afanasyev.26@gmail.com>
2023-02-26 19:20:54 +0300

    05 problem has been fixed; appropriate README has been uploaded

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,17 +1,20 @@
 #include <algorithm>
+#include <cstdint>
 #include <iostream>
 
 int main()
 {
 	int N;
 	std::cin >> N;
-	int * letters = new int[N];
+	int previous = -1, current = 0;
+	int64_t count = 0;
 	for (auto i = 0; i < N; i++)
-		std::cin >> letters[i];
-	auto max_goodness = 0;
-	for (auto i = 0; i < N - 1; i++)
-		max_goodness += std::min(letters[i], letters[i + 1]);
-	std::cout << max_goodness << std::endl;
-	delete [] letters;
+	{
+		std::cin >> current;
+		if (previous != -1)
+			count += std::min(previous, current);
+		previous = current;
+	}
+	std::cout << count << std::endl;
 	return 0;
 }

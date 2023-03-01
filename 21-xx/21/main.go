d0f4fd8984ce0cc1c3ca952a805d2ff1d090aec1
Ilya Afanasyev <affeeal@yandex.ru>
2023-03-02 01:19:01 +0300

    21 problem has been uploaded

--- 21-xx/21/main.go
+++ 21-xx/21/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
+	var N int
+	fmt.Scan(&N)
+	size := int(math.Max(float64(N), 3))
+	dp := make([]int, size);
+	dp[0] = 2;
+	dp[1] = 4;
+	dp[2] = 7;
+	for i := 3; i < N; i++ {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+	}
+	fmt.Println(dp[N - 1])
 }
